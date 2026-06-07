07e8bdbc180264fc10c96e602d6f11ac1c1dc806
Sho Sakaue <sho.sakaue0808@gmail.com>
2026-06-07 13:53:10 -0600

    write data preprocessing for GSM dataset and changed folder structures

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,58 +1,49 @@
-"pre-processing dataset for training and evaluation"
+"""
+pre-processing dataset for training and evaluation. The dataset is in the form of question and answer pairs.
+We will tokenize the questions and answers separately, and then concatenate them together with padding
+to create the input for the model. We will also create an attention mask 
+to indicate which tokens are contributing to the loss 
+Mostly referenced from the source code of GSM8K dataset repository:
+https://github.com/openai/grade-school-math
+"""
+
+import torch
+
+
 # Data preprocessing
-class DollyDataset(Dataset):
-    def __init__(self, dataset, tokenizer, max_length):
+class GSMDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset, tokenizer):
         self.dataset = dataset
         self.tokenizer = tokenizer
-        self.max_length = max_length
+        # extract question and answer pairs from the dataset
+        self.qns = dataset["question"][:len(dataset)]
+        self.ans = dataset["answer"][:len(dataset)]
+        self.qns = tokenizer(self.qns, padding=False, return_length=True) # tokenize questions without padding
+        self.ans = tokenizer(self.ans, padding=False, return_length=True) # tokenize answers without padding
+        self.max_len = max(
+            [
+                self.qns["length"][i] + self.ans["length"][i]
+                for i in range(len(self.dataset))
+            ]
+        )
+        print(f"Max tokens: {self.max_len}")
 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, idx):
-        ex = self.dataset[idx]
-        instruction = ex["instruction"].strip()
-        context = ex.get("context", "").strip()
-        response = ex["response"].strip()
+        qn_tokens = self.qns["input_ids"][idx]
+        ans_tokens = self.ans["input_ids"][idx]
+        pad_tokens = [0] * (self.max_len - len(qn_tokens) - len(ans_tokens))
+        tokens = qn_tokens + ans_tokens + pad_tokens
+        mask = [0] * len(qn_tokens) + [1] * len(ans_tokens) + [0] * len(pad_tokens)
 
-        if context:
-            prompt = (
-                f"### Instruction:\n{instruction}\n\n"
-                f"### Context:\n{context}\n\n"
-                f"### Response:\n"
-            )
-        else:
-            prompt = f"### Instruction:\n{instruction}\n\n### Response:\n"
-
-        full_text = prompt + response
-
-        full_enc = self.tokenizer(
-            full_text,
-            truncation=True,
-            max_length=self.max_length,
-            padding="max_length",
-            return_tensors="pt",
-        )
-        prompt_enc = self.tokenizer(
-            prompt,
-            truncation=True,
-            max_length=self.max_length,
-            padding="max_length",
-            return_tensors="pt",
-        )
-        # make it vector
-        input_ids = full_enc["input_ids"].squeeze(0)
-        attention_mask = full_enc["attention_mask"].squeeze(0)
-
-        # labels for causal LM (autoregressive)
-        labels = input_ids.clone()
-        prompt_len = int(prompt_enc["attention_mask"].sum())
-        # assign -100 for parts except for response tokens so that loss will be calculated just by response tokens
-        labels[:prompt_len] = -100
-        labels[attention_mask == 0] = -100
-
+        tokens = torch.tensor(tokens)
+        mask = torch.tensor(mask)
         return {
-            "input_ids": input_ids,
-            "attention_mask": attention_mask,
-            "labels": labels,
+            "input_ids": tokens,
+            "attention_mask": mask,
         }
+    
+    def __str__(self):
+        return f"GSMDataset with {len(self.dataset)} examples, max tokens: {self.max_len}" 
